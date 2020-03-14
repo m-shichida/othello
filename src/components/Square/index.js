fdb8d174f1c672574f0d442192275e4e225c2302
m-shichida <42334697+m-shichida@users.noreply.github.com>
2020-03-14 19:41:36 +0900

    development (#1)
    * 裏返しできるようにする
    
    * 各コマのカウントができるようにする

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -3,7 +3,25 @@ import styles from './styles'
 
 const { SquareButton } = styles
 
-const Square = ({ piece, addValue }) => {
+const BlackPiece = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <circle cx="24" cy="24" r="20" stroke="black" fill="black" />
+    </svg>
+  )
+}
+
+const WhitePiece = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <circle cx="24" cy="24" r="20" stroke="black" fill="white" />
+    </svg>
+  )
+}
+
+const Square = ({ square, addValue }) => {
+  const piece = square === 1 ? <BlackPiece /> : square === 2 ? <WhitePiece /> : ''
+
   return (
     <SquareButton
       onClick={ addValue }
